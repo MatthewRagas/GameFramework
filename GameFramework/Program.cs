82bc3d3d60cb57cf64e922866d57a3eac2367c68
Matthew John Ragas <mjrag310@hotmail.com>
2019-10-18 09:38:34 -0500

    Created Vec2 and Vec3 classes

--- GameFramework/Program.cs
+++ GameFramework/Program.cs
@@ -9,7 +9,7 @@ namespace GameFramework
     class Program
     {
         static void Main(string[] args)
-        {
+        {            
             Game game = new Game();
             game.Run();
         }
---
+++ GameFramework/Vector3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFramework
+{
+    class Vector3
+    {
+        private float _x, _y, _z;
+
+        public Vector3(float x,float y,float z)
+        {
+            _x = x;
+            _y = y;
+            _z = y;
+        }
+
+        public static Vector3 operator +(Vector3 lhs, Vector3 rhs)
+        {
+            return new Vector3(lhs._x + rhs._x, lhs._y + rhs._y, lhs._z + rhs._z);
+        }
+
+        public static Vector3 operator -(Vector3 lhs, Vector3 rhs)
+        {
+            return new Vector3(lhs._x - rhs._x, lhs._y - rhs._y, lhs._z - rhs._z);
+        }
+
+        public static Vector3 operator /(Vector3 lhs, float num)
+        {
+            return new Vector3(lhs._x / num, lhs._y / num, lhs._z / num);
+        }
+
+        public static Vector3 operator /(float num, Vector3 rhs)
+        {
+            return new Vector3(num / rhs._x, num / rhs._y, num / rhs._z);
+        }
+
+        public static Vector3 operator *(Vector3 lhs, float num)
+        {
+            return new Vector3(lhs._x * num, lhs._y * num, lhs._z * num);
+        }
+
+        public static Vector3 operator *(float num, Vector3 rhs)
+        {
+            return new Vector3(num * rhs._x, num * rhs._y, num * rhs._z);
+        }
+    }
+}
